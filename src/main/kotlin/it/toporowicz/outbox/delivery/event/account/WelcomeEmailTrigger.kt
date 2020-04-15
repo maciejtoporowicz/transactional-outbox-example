46657e2eff1b83a1d8bb52cffa300615a3b5033d
maciekt <maciej.toporowicz@gmail.com>
2020-04-15 20:45:53 +0200

    correct log

--- src/main/kotlin/it/toporowicz/outbox/delivery/event/account/WelcomeEmailTrigger.kt
+++ src/main/kotlin/it/toporowicz/outbox/delivery/event/account/WelcomeEmailTrigger.kt
@@ -1,13 +1,16 @@
 package it.toporowicz.outbox.delivery.event.account
 
 import it.toporowicz.outbox.core.account.AccountCreatedEvent
+import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class WelcomeEmailTrigger {
+    private val log = LoggerFactory.getLogger(WelcomeEmailTrigger::class.java)
+
     @EventListener
     fun onAccountCreated(event: AccountCreatedEvent) {
-        println("Sending email according to $event")
+        log.info("Received event: $event")
     }
 }
