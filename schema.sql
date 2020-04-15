7b7d95ef5cdeb788ed94fa12f6e45a55d2cccbd1
maciekt <maciej.toporowicz@gmail.com>
2020-04-15 20:51:50 +0200

    fix schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 CREATE TABLE outbox_message (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     type varchar(100) NOT NULL,
-    content varchar(512) NOT NULL,
+    json_content varchar(512) NOT NULL,
+    primary key (id)
 );
 ﻿
 CREATE TABLE account (
-    id varchar(36) PRIMARY KEY,
-    name varchar(50)
+    id varchar(36),
+    name varchar(50),
+	email varchar(100),
+    primary key (id)
 );
